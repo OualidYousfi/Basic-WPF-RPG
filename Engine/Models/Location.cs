7dc25fa1621606363dd9c6093940f39a1589ebba
OualidYousfi <oualid.yousfi@gmail.com>
2019-02-23 13:30:07 +0100

    4.1 creating Location class

---
+++ Engine/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class Location
+    {
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String ImageName { get; set; }
+    }
+}
